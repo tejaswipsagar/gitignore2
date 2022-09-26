d87dc4236a2c05fda38096d5399f5488ce79b2df
TEJASWI P <tejaswip811@gmail.com>
2022-09-26 18:39:28 +0530

    First Commit

---
+++ src/contact_details.js
@@ -0,0 +1,24 @@
+// All the contact details that is stored here
+
+var contact_details = {
+  // First person details
+  name1 : "Tejaswi Sagar",
+  ph_no1: "+91 6363151733",
+  e_mail1: "tejaswip811@gmail.com",
+  img1 : "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+
+   // second person details
+   name2 : "Samaksha",
+   ph_no2: "+91 7019492868",
+   e_mail2: "samaksha225@gmail.com",
+   img2: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+
+    // third person details
+  name3 : "Jane Smith",
+  ph_no3: "+91 9449882191",
+  e_mail3: "janesmith123@gmail.com",
+  img3: "http://portfolio.newschool.edu/guptm392/files/2014/08/DSC_1004-2-1a1yqd6.jpg"
+
+}
+
+export {contact_details};
